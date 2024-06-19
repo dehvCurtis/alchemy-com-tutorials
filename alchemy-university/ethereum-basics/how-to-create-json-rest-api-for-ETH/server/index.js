67d3d7e6757b78601ac2767985deed7d537b093b
Dehvon Curtis <dcurtis@datto.com>
2024-06-19 09:10:00 -0600

    commit

--- alchemy-university/ethereum-basics/how-to-create-json-rest-api-for-ETH/server/index.js
+++ alchemy-university/ethereum-basics/how-to-create-json-rest-api-for-ETH/server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const app = express()
+const app = express();
 const port = 3000;
+const things = ["this is a thing","also this"];
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.send(things);
 });
 
 app.listen(port, () => {
