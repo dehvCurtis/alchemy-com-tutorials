1160fad4d0afc3070ad709d16d932bac8ee97e2d
Dehvon Curtis <dcurtis@datto.com>
2024-06-19 09:11:03 -0600

    added post

--- alchemy-university/ethereum-basics/how-to-create-json-rest-api-for-ETH/server/index.js
+++ alchemy-university/ethereum-basics/how-to-create-json-rest-api-for-ETH/server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const things = ["this is a thing","also this"];
+const things = [];
 
 app.get('/', (req, res) => {
     res.send(things);
 });
+
+app.post('/', (req, res) => {
+    things.push(req.body);
+    res.send(things);
+  });
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
